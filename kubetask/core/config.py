9e0c82bdcd438397fa2f9cbf8abd2719b6235075
Bhavani Ravi <bhava0895@gmail.com>
2020-12-19 05:46:48 +0530

    add config based on env

--- kubetask/core/config.py
+++ kubetask/core/config.py
@@ -1,2 +1,21 @@
-class Config:
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class BaseConfig:
     pass
+
+class LocalConfig:
+    pass
+
+class Config(BaseConfig):
+    pass
+
+class UTConfig(BaseConfig):
+    KUBETASK_DB = "postgresql://postgres:123@localhost:5432/kubetask"
+
+
+def get_config():
+    env = os.environ["ENV"]
+    return globals()[f"{env}Config"]
